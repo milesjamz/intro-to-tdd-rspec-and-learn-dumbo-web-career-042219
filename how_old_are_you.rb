ddde5660b345a72f56881bd4b186eaf7ef287bb9
mjm03 <mjm03@hampshire.edu>
2019-03-24 12:53:41 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,4 +1,4 @@
-```ruby
+ruby
 require_relative './current_age_for_birth_year.rb'
 
 puts "What year were you born?"
@@ -7,4 +7,3 @@ birth_year = gets.to_i
 users_age = current_age_for_birth_year(birth_year)
 
 puts "You are: " + users_age.to_s + " years old."
-```
